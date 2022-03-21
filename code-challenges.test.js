cc68741e79e3726c438f91b7a258ad27defb69b3
Omar Bowen <bronxkidd95@gmail.com>
2022-03-20 20:05:47 -0700

    answers to challenge questiosn

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,17 +15,17 @@
 
 // a) Create a test with an expect statement using the variable provided. HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
-// Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
-// Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
-
-describe("removeFirstItem", () => {
-  it ("takes in an array, removes the first item from the array and shuffles the remaining content", () => {
-  expect(removeFirstItem(colors1)).toEqual(expect.arraycontainingarray(["purple","blue","green","yellow","pink"]))
-  expect(removeFirstItem(colors2)).toEqual(expect.arraycontainingarray(["chartreuse","indigo","periwinkle","ochre","aquamarine","saffron"]))
-  })
-})
+// const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+// // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+// const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+// // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+//
+// describe("removeFirstItem", () => {
+//   it ("takes in an array, removes the first item from the array and shuffles the remaining content", () => {
+//   expect(removeFirstItem(colors1)).toEqual(expect.arraycontainingarray(["purple","blue","green","yellow","pink"]))
+//   expect(removeFirstItem(colors2)).toEqual(expect.arraycontainingarray(["chartreuse","indigo","periwinkle","ochre","aquamarine","saffron"]))
+//   })
+// })
 
 // b) Create the function that makes the test pass.
 
@@ -40,35 +40,35 @@ describe("removeFirstItem", () => {
 // return new array
 
 
-const removeFirstItem = (array) => {
-  let n = array.length
-  let newArr =[]
-  array.shift()
-  while (array.length >0) {
-    let randomnum = Math.floor(Math.random() * n--)
-    let spliceRandomly= array.splice(randomNum, 1)
-    newArr.push(spliceRandomly)
-}
-return newArr.flat()
-
-}
+// const removeFirstItem = (array) => {
+//   let n = array.length
+//   let newArr =[]
+//   array.shift()
+//   while (array.length >0) {
+//     let randomnum = Math.floor(Math.random() * n--)
+//     let spliceRandomly= array.splice(randomNum, 1)
+//     newArr.push(spliceRandomly)
+// }
+// return newArr.flat()
+//
+// }
 
 
 // --------------------2) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const nums1 = [3, 56, 90, -8, 0, 23, 6]
-// Expected output: [-8, 90]
-const nums2 = [109, 5, 9, 67, 8, 24]
-// Expected output: [5, 109]
-
-describe("minAndMax", () => {
-  it ("takes an array of numbers and returns an array of the minimum and maximum numbers in that order", () => {
-  expect(minAndMax(nums1)).toEqual(["-8","90"])
-  expect(minAndMax(nums2)).toEqual(["5","109"])
-  })
-})
+// const nums1 = [3, 56, 90, -8, 0, 23, 6]
+// // Expected output: [-8, 90]
+// const nums2 = [109, 5, 9, 67, 8, 24]
+// // Expected output: [5, 109]
+// //
+// describe("minAndMax", () => {
+//   it ("takes an array of numbers and returns an array of the minimum and maximum numbers in that order", () => {
+//   expect(minAndMax(nums1)).toEqual(["-8","90"])
+//   expect(minAndMax(nums2)).toEqual(["5","109"])
+//   })
+// })
 
 // b) Create the function that makes the test pass.
 
@@ -77,8 +77,19 @@ describe("minAndMax", () => {
 // declare two variables
 // min variable takes array with spread operator and uses Math.min
 // max variable takes in array with spread operator and uses Math.max
-// push numbers into a new array
 // return array
+
+// const minAndMax = (array) => {
+//   let nums1= [3, 56, 90, -8, 0, 23, 6]
+//   let nums2= [109, 5, 9, 67, 8, 24]
+//
+//   return Math.min([...array])
+//   return Math.max([...array])
+// }
+//   console.log(minAndMax(nums1))
+//   console.log(minAndMax(nums2))
+
+
 
 
 
@@ -101,9 +112,18 @@ describe("noDuplicates", () => {
 
 //declare a function named noDuplicates that takes in two params of array one and array two
 // within func declare a var called combined arrays
-// set equal rray one and concatenated with array 2
-// decalre a variable called unique arrays
-// set that equal to the combined array with a filter mehtod
+// set equal array one and concatenated with array 2
+// declare a variable called unique arrays
+// set that equal to the combined array with a filter method
 // use the arguments value and index inside the code block
 // return only values where the first index of a value only equales the current index
 //return the specific array
+
+const noDuplicates = (array) => {
+
+  var combinedArrays = testArray1.concat(testArray2)
+  var uniqueArrays = combinedArrays
+
+  return uniqueArrays.filter((value, index))
+}
+  console.log(noDuplicates(uniqueArrays))
